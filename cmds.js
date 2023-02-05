96fc27a95b18e817ec6ba24a634f65d2beb9d692
David Greitan <alphabaqpla@outlook.com>
2023-02-05 20:28:14 +0300

    update

---
+++ cmds.js
@@ -0,0 +1,57 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js')
+const config = require('./config.json')
+function del(msg,client){
+    if (!msg.member.permissions.has("ADMINISTRATOR")) return msg.channel.send({content: '**Вы не можете использовать команды администратора.**'})
+    else if(msg.channel.parentId==config.Tickets.Parent1.ChannelParent || msg.channel.parentId==config.Tickets.Parent2.ChannelParent || msg.channel.parentId==config.Tickets.Parent3.ChannelParent){
+        function getId(id){
+            var t = client.db.get(id)
+            return t
+        }
+        const embed = new EmbedBuilder()
+        .setColor('#00bd6d')
+        .setAuthor({
+            name: 'Удаление тикета!'
+        })
+        .setDescription('**Тикет будет безвозвратно удалён через 30 секунд!**')
+        .setThumbnail(config.thumbImage)
+        .setFooter({
+            text: config.footerText
+        })
+        const row = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+            .setCustomId('deletestop')
+            .setLabel('отменить')
+            .setEmoji('☺️')
+            .setStyle(ButtonStyle.Success),
+        )
+        msg.reply({
+            embeds: [embed],
+            components: [row]
+        })
+        client.db.set(msg.channel.id,'delete')
+        setTimeout(() => {
+            if(getId(msg.channel.id)=='delete'){
+                msg.channel.delete()
+            }
+            else{
+                msg.channel.send({
+                    content: "Канал не удалён"
+                })
+            }
+        }, 30000)
+    }
+    else{
+        msg.channel.send({
+            content:'**Этот канал нельзя удалить.**'
+        })
+    }
+}
+var comms_list = [
+  {
+    name: "del",
+    out: del,
+    about: "Удалить канал."
+  }
+]
+module.exports.comms = comms_list
--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-//============-NewTickets BY Alpha-============
-//===================-VAR`S-===================
-const fs = require('fs')
-const {Client, MessageEmbed, Intents, MessageActionRow, MessageButton} = require('discord.js');
-const client = new Client({ intents: 32767 })
-const discordModals = require('discord-modals')
-discordModals(client)
-const Discord = require('discord.js')
+//=========-TicketsSystem BY Alpha-==========
+//==================-VAR`S-==================
 const config = require('./config.json')
-client.discord = Discord
-client.config = config 
-const token = config.token
-//=============-DISCORD BOT LOGIN-=============
-client.login(token)
-//===================-EVENTS-==================
+const { Client,GatewayIntentBits} = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.MessageContent,GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.GuildMembers,GatewayIntentBits.GuildIntegrations]})
+const db = require('qjson-db')
+client.db = new db('./data/DB.json')
+client.config = config
+const fs = require('fs')
+//=================-DISCORD-==================
 const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
 for (const file of events){
     const event = require(`./events/${file}`)
     client.on(event.name, (...args) => event.execute(...args, client))
 }
 client.on('messageCreate', (msg) => {
-  const prefix = config.prefix
-    const comms = require("./comms.js")
-    if (msg.author.username != client.user.username && msg.author.discriminator != client.user.discriminator) {
+    const comms = require("./cmds.js")
+    if (msg.author.username != client.user.username && msg.author.discriminator != client.user.discriminator){
       var comm = msg.content.trim() + " "
-      var comm_name = comm.slice(0, comm.indexOf(" "));
-      var messArr = comm.split(" ");
+      var comm_name = comm.slice(0, comm.indexOf(" "))
       for (comm_count in comms.comms) {
-        var comm2 = prefix + comms.comms[comm_count].name;
-        if (comm2 == comm_name) {
-          comms.comms[comm_count].out(client, msg, messArr);
+        var comm2 = config.prefix + comms.comms[comm_count].name
+        if (comm2 == comm_name){
+          comms.comms[comm_count].out(msg,client)
         }
       }
     }
   }
 )
+client.login(config.token)
+//===================-MAIN-===================
